8bbe8d4f94859f6b23e8bed09175a02924ea5124
Wiamer <erem.amarachi@gmail.com>
2020-05-09 22:53:24 +0100

    Update game.js

--- game.js
+++ game.js
@@ -108,7 +108,7 @@ getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= Max_questions) {
         localStorage.setItem("mostRecentScore", score);
         // go to the end page
-        return window.location.assign('./end.html');
+        return window.location.assign("./end.html");
     }
     questionCounter++;
     questionCounterText.innerText = `${questionCounter}/${Max_questions}`;
